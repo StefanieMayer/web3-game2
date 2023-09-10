b37ddad40deca048e2a141f49dc713415bae8a9f
Stefanie <stefanie.s.mayer@gmail.com>
2023-09-10 11:10:58 +0200

    Initial Commit

--- pages/game.js
+++ pages/game.js
@@ -15,7 +15,7 @@ export default function game(props){
             </Head>
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                    Welcjjjhome to <a href="https://nextjs.org">Next.js!</a>
                 </h1>
                 <div><PixiCanvas/></div>
             </main>
