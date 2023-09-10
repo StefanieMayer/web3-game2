d3db8556e6f591d6d159e5b0c797278c60d0e345
Stefanie <stefanie.s.mayer@gmail.com>
2023-09-10 19:32:52 +0200

    Version I

---
+++ pages/esg.js
@@ -0,0 +1,62 @@
+const contact = [
+
+    {
+    name: "VAKA Health Foundation", 
+    imgURL: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTSXBxXx0Zj6PXtoG_nVuHAm-tBkpY0TFW6C6qF7rRpu4bFFkHD",
+    URL: "https://vakahealthfoundation.org/"
+    
+    shortDescription: "The VAKA Foundation builds a social enterprise for Healthcare workers.",
+    longDescription: "They provide a medical professional education to ensure universal accessibility and effective communication on trending healthcare matters.",
+    
+    // donate: https://vakahealthfoundation.org/donate-noww/
+    // award: AAMEG
+    },
+    
+    
+    {
+    name: "e.s.g. Transparency Initiative",
+    imgURL: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTH8Bv41RNhdLHYOi-lw0SALqP51F5-hTHrTpO1uCSxKdcxLk2y",
+    URL: "https://www.esg-transparency.org/",
+    
+    shortDescription: "The ESG Transparecy Initiative is a human rights organization fighting for a more respectful enviroment.",
+    longDescription: "They are building a movement of investors and consumers who can drive corporate social responsibility from the bottom up. They achieved its first campaing win in 2018 when they pushed one of the country's largest pension fund to divest from for-profit companys over their human rights abuses of migrants.",
+    
+    //Donate via Paypal
+    },
+    
+    {
+    name: "Malampaya Foundation",
+    imgURL: "https://www.malampayafoundation.org/",
+    URL: "https://assets.website-files.com/60f06a88403509115b3328b5/6152d428362df07c4d0b4475_Malampaya%20Foundation%20-%20OG.jpg",
+    
+    shortDescription: "Marine Biodiversity Conservation helping alleviate poverty.",
+    longDescription: "The foundation empowers people by provision of tools to enable productivity, social and economic development to protect the enviroment.",
+    
+    //awarded: 2002 World Summit Business Award at Word Summit on Sustainable Development held in Johannisburg, South Africa
+    },
+    
+    
+    {
+    name: "mercyHousing",
+    imgURL: "https://www.mercyhousing.org/",
+    URL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDp_HuPaqrJ_REZ7rWXRAmYm2TMng-PQBu-vkUusW1huFladRS",
+    
+    shortDescription: "Mercy Housing transforming lives through affordable housing.",
+    longDescription: "With your donation you provide affordable housing to vulnerable people, to build up a new live including health education, after-school support and financial literacy classes.",
+    },
+    
+    
+    {
+    name: "Pro Bono SG",
+    imgURL: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSJ5qeytNQCBizkPkFrsmSfLSo6JJWR_gynB9FFIHULIvlyCVxe",
+    URL: "https://www.probono.sg/esg/",
+    
+    shortDescription: "Pro Bono Singapore helps to access to justice when you cannot afford a lawyer.",
+    longDescription: "Lorem ipsum.",
+    
+    //Donate: https://www.probono.sg/get-involved/donate/
+    // award: charity transparancy Award 2022
+    },
+    
+    ];
+    
